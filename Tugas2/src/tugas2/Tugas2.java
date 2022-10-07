5a641a1df796f53d0110bff1c3e21002a0763ad7
fikriamarayana <106722259+fikriamarayana@users.noreply.github.com>
2022-10-07 17:57:05 +0700

    Add files via upload

---
+++ Tugas2/src/tugas2/Tugas2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas2;
+
+/**
+ *
+ * @author fikria
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float jmlPengulangan=10;
+        for(float i=0;i<jmlPengulangan;i++){
+            if (i%2==0){
+                System.out.println("Genap");
+            }else{
+              System.out.println("Ganjil");
+            }  
+            }
+    }
+    
+}
